3bfcf7989b907aa636c30c0600f5e113c79f1ab4
aaboundi <aadja@gmail.com>
2022-08-25 09:12:55 -0700

    binary100-correct

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,7 +4,7 @@
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: a pointer to the first node to find the ancestor
  * @second: a pointer to the second node to find the ancestor
- * 
+ *
  * Return: pointer to the ancestor node
  *         NULL if there is no ancestor node
  */
