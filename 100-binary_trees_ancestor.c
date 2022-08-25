291d7a232c36e0a23dd100d9ec13a3ab57f9c8b3
aaboundi <aadja@gmail.com>
2022-08-25 09:16:05 -0700

    binary100-first

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 {
 	size_t depth_first, depth_second;
 
-	if (!frist || !second)
+	if (!first || !second)
 		return (NULL);
 
 	depth_first = binary_tree_depth(first);
